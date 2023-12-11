f75f0526dc60cd7c52094552318250b82cde7b73
Erlendns <132302724+Erlendns@users.noreply.github.com>
2023-12-11 19:54:22 +0100

    Update ph_waterLevel_waterTemp_sensor_11dec.ino

--- Vann Kvalitet/ph_waterLevel_waterTemp_sensor_09dec.ino
+++ Vann Kvalitet/ph_waterLevel_waterTemp_sensor_09dec.ino
@@ -1,4 +1,11 @@
-// This program measures the pH level, water temperature, and water level and sends the information to NodeRED
+/*
+  This program measures the pH level, water temperature, and water level and sends the information to RPi.
+
+  Components used in water quality sensor:
+  ESP32 - https://tinyurl.com/756y9hvf
+  pH and temp sensor - https://www.aliexpress.com/item/1005001612930856.html?spm=a2g0o.order_list.order_list_main.25.21ef180247h6p0
+  Water level sensor - https://www.aliexpress.com/item/1942293819.html?src=google&src=google&albch=shopping&acnt=897-548-8760&slnk=&plac=&mtctp=&albbt=Google_7_shopping&gclsrc=aw.ds&albagn=888888&ds_e_adid=&ds_e_matchtype=&ds_e_device=c&ds_e_network=x&ds_e_product_group_id=&ds_e_product_id=en1942293819&ds_e_product_merchant_id=106536705&ds_e_product_country=NO&ds_e_product_language=en&ds_e_product_channel=online&ds_e_product_store_id=&ds_url_v=2&albcp=19996259659&albag=&isSmbAutoCall=false&needSmbHouyi=false&gad_source=1&gclid=CjwKCAiAg9urBhB_EiwAgw88mbCva0zwm4iS-277eawWMPP9Al3sE0Kir1yAZsdaLnPqrsfGYKFKZRoCRWEQAvD_BwE&aff_fcid=ec10fb19036c4d9d86e80f1fb6e6d828-1702320600780-05746-UneMJZVf&aff_fsk=UneMJZVf&aff_platform=aaf&sk=UneMJZVf&aff_trace_key=ec10fb19036c4d9d86e80f1fb6e6d828-1702320600780-05746-UneMJZVf&terminal_id=a343b62e118f4ce398462bb0ce8c7f6f&afSmartRedirect=y
+*/
 
 // Millis
 unsigned long previousMillis = 0;
